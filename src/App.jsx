2ba4bb637cc9996480e50154dcd7b8f3cf4794d8
nikitaacodes <nikitaa4822@gmail.com>
2025-02-22 17:01:50 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  return <div className="bg-red-200">App</div>;
+};
 
-export default App
+export default App;
